ecdfe6704249208bf027e3f9915429f23e862167
Stabbles1 <adam.poole93@gmail.com>
2021-12-11 12:58:12 +0000

    Formatting

--- src/day_10/program.py
+++ src/day_10/program.py
@@ -7,20 +7,20 @@ from chunk_character import ChunkCharacter
 class Program:
     def __init__(self, chunk_lines: List[ChunkLine]):
         self.chunk_lines = chunk_lines
-        self.corrupt_tally = corrupt_tally = {
+
+    def calculate_corrupt_score(self):
+        corrupt_tally = {
             ")": ChunkCharacter("(", ")", 3),
             "]": ChunkCharacter("[", "]", 57),
             "}": ChunkCharacter("{", "}", 1197),
             ">": ChunkCharacter("<", ">", 25137),
         }
-
-    def calculate_corrupt_score(self):
         corrupt_lines = [line for line in self.chunk_lines if line.is_corrupt()]
         for corrupt_line in corrupt_lines:
-            self.corrupt_tally[corrupt_line.corrupt_character].increment()
+            corrupt_tally[corrupt_line.corrupt_character].increment()
 
         total = 0
-        for corrupt_char in self.corrupt_tally.values():
+        for corrupt_char in corrupt_tally.values():
             total += corrupt_char.corrupt_points * corrupt_char.count
         return total
 
@@ -31,10 +31,8 @@ class Program:
         for line in self.chunk_lines:
             scores.append(line.completion_score())
         scores.sort()
-        return scores[int(len(scores)/2)]
+        return scores[int(len(scores) / 2)]
 
-
-        
     def remove_corrupt_lines(self):
         for line in list(self.chunk_lines):
             if line.is_corrupt():
--- tests/day_10/test_10.py
+++ tests/day_10/test_10.py
@@ -2,17 +2,18 @@ from day_10.main import input_to_program
 from day_10.chunk_line import ChunkLine
 
 test_input = [
-"[({(<(())[]>[[{[]{<()<>>",
-"[(()[<>])]({[<{<<[]>>(",
-"{([(<{}[<>[]}>{[]{[(<()>",
-"(((({<>}<{<{<>}{[]{[]{}",
-"[[<[([]))<([[{}[[()]]]",
-"[{[{({}]{}}([{[{{{}}([]",
-"{<[[]]>}<{[{[{[]{()[[[]",
-"[<(<(<(<{}))><([]([]()",
-"<{([([[(<>()){}]>(<<{{",
-"<{([{{}}[<[[[<>{}]]]>[]]",
+    "[({(<(())[]>[[{[]{<()<>>",
+    "[(()[<>])]({[<{<<[]>>(",
+    "{([(<{}[<>[]}>{[]{[(<()>",
+    "(((({<>}<{<{<>}{[]{[]{}",
+    "[[<[([]))<([[{}[[()]]]",
+    "[{[{({}]{}}([{[{{{}}([]",
+    "{<[[]]>}<{[{[{[]{()[[[]",
+    "[<(<(<(<{}))><([]([]()",
+    "<{([([[(<>()){}]>(<<{{",
+    "<{([{{}}[<[[[<>{}]]]>[]]",
 ]
+
 
 def test_input_to_input_to_program():
     program = input_to_program(test_input)
@@ -20,31 +21,37 @@ def test_input_to_input_to_program():
     assert program.chunk_lines[0].line == "[({(<(())[]>[[{[]{<()<>>"
     assert program.chunk_lines[4].line == "[[<[([]))<([[{}[[()]]]"
 
+
 def test_chunk_line_can_identify_corrupt():
-    valid_line  = ChunkLine("[({(<(())[]>[[{[]{<()<>>")
-    corrupt_line  = ChunkLine("{([(<{}[<>[]}>{[]{[(<()>")
+    valid_line = ChunkLine("[({(<(())[]>[[{[]{<()<>>")
+    corrupt_line = ChunkLine("{([(<{}[<>[]}>{[]{[(<()>")
 
     assert valid_line.is_corrupt() == False
     assert corrupt_line.is_corrupt() == True
     assert corrupt_line.corrupt_character == "}"
 
+
 def test_part_1():
     program = input_to_program(test_input)
-    assert(program.calculate_corrupt_score()) == 26397
+    assert (program.calculate_corrupt_score()) == 26397
+
 
 def test_program_can_remove_corrupt_lines():
     program = input_to_program(test_input)
     program.remove_corrupt_lines()
     assert len(program.chunk_lines) == 5
 
+
 def test_chunk_line_can_complete_sequence():
-    incomplete_line  = ChunkLine("[({(<(())[]>[[{[]{<()<>>")
+    incomplete_line = ChunkLine("[({(<(())[]>[[{[]{<()<>>")
     assert incomplete_line.get_completion_sequence() == "}}]])})]"
 
+
 def test_chunk_line_can_calculate_completion_score():
-    incomplete_line  = ChunkLine("<{([{{}}[<[[[<>{}]]]>[]]")
+    incomplete_line = ChunkLine("<{([{{}}[<[[[<>{}]]]>[]]")
     assert incomplete_line.get_completion_sequence() == "])}>"
     assert incomplete_line.completion_score() == 294
+
 
 def test_part_2():
     program = input_to_program(test_input)
